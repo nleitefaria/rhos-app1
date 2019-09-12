4cfed56eebfd332e9bd48499d8b52b474d564bae
nleitefaria <nleitefaria@gmail.com>
2019-09-12 19:24:40 +0100

    INIT

---
+++ src/main/java/com/mycompany/rhosapp1/SwaggerJaxrsResourceConfiguration.java
@@ -0,0 +1,27 @@
+package com.mycompany.rhosapp1;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+import com.mycompany.rhosapp1.rws.PingResource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/resources")
+public class SwaggerJaxrsResourceConfiguration extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet<>();
+        resources.add(com.wordnik.swagger.jaxrs.listing.ApiListingResource.class);
+        resources.add(com.wordnik.swagger.jaxrs.listing.ApiDeclarationProvider.class);
+        resources.add(com.wordnik.swagger.jaxrs.listing.ApiListingResourceJSON.class);
+        resources.add(com.wordnik.swagger.jaxrs.listing.ResourceListingProvider.class);
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(PingResource.class);
+    }
+}
