073473a3546b1ceebf31f03f0f59ddc5d489de18
nleitefaria <nleitefaria@nleitefaria-HP-EliteBook>
2019-09-30 20:28:47 +0100

    Added pagination to Customers

--- src/main/java/com/mycompany/rhosapp1/rws/CustomerResource.java
+++ src/main/java/com/mycompany/rhosapp1/rws/CustomerResource.java
@@ -5,11 +5,13 @@ import javax.json.JsonBuilderFactory;
 import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.mycompany.rhosapp1.service.impl.CustomersServiceImpl;
+import com.mycompany.rhosapp1.service.impl.ProductsServiceImpl;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
@@ -22,9 +24,9 @@ public class CustomerResource {
     @Produces(MediaType.APPLICATION_JSON)
     @ApiOperation(value = "count", notes = "Get customers count")
     public Response count() {
-		CustomersServiceImpl customersService = new CustomersServiceImpl();
+		CustomersServiceImpl service = new CustomersServiceImpl();
         JsonBuilderFactory factory = Json.createBuilderFactory(null);
-        JsonObject value = factory.createObjectBuilder().add("count", customersService.count()).build();
+        JsonObject value = factory.createObjectBuilder().add("count", service.count()).build();
         return Response.ok(value).build();
     }
 	
@@ -33,8 +35,18 @@ public class CustomerResource {
     @Produces(MediaType.APPLICATION_JSON)
     @ApiOperation(value = "all", notes = "Get all customers")
     public Response findAll() {
-		CustomersServiceImpl customersService = new CustomersServiceImpl();
-        return Response.status(200).entity(customersService.findAll()).build();
+		CustomersServiceImpl service = new CustomersServiceImpl();
+        return Response.status(200).entity(service.findAll()).build();
     }
+	
+	@GET
+	@Path("/all/{pageNum}")
+	@Produces({MediaType.APPLICATION_JSON})
+	@ApiOperation(value = "all pagination", notes = "Get all customers with pagination")
+	public Response findCustomersPage(@PathParam("pageNum") Integer pageNum)
+	{
+		CustomersServiceImpl service = new CustomersServiceImpl();
+		return Response.status(200).entity(service.findPage(pageNum)).build();
+	}
 
 }
--- src/main/java/com/mycompany/rhosapp1/service/impl/CustomersServiceImpl.java
+++ src/main/java/com/mycompany/rhosapp1/service/impl/CustomersServiceImpl.java
@@ -9,35 +9,51 @@ import javax.persistence.Persistence;
 
 import com.mycompany.rhosapp1.dao.CustomersDAO;
 import com.mycompany.rhosapp1.dto.CustomersDTO;
+import com.mycompany.rhosapp1.dto.CustomersPageDTO;
 import com.mycompany.rhosapp1.entity.Customers;
 import com.mycompany.rhosapp1.service.CustomersService;
 
 @Stateless
 public class CustomersServiceImpl implements CustomersService{
 	
-	CustomersDAO customersDAO;
+	CustomersDAO dao;
 	
 	public CustomersServiceImpl()
 	{
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("RHOSAPP1PU");
-		this.customersDAO = new CustomersDAO(emf);
+		this.dao = new CustomersDAO(emf);
 	}
 	
 	public Integer count()
 	{
-		return this.customersDAO.getCustomersCount();
+		return this.dao.getCustomersCount();
 	}
 	
 	public List<CustomersDTO> findAll()
 	{
 		List<CustomersDTO> ret = new ArrayList<CustomersDTO>();	
-		for(Customers c : customersDAO.findCustomersEntities())
+		for(Customers c : dao.findCustomersEntities())
 		{
 			ret.add(new CustomersDTO(c.getId(), c.getCompany(), c.getLastName(), c.getFirstName(), c.getEmailAddress(), c.getJobTitle(), c.getBusinessPhone(), c.getHomePhone(), c.getMobilePhone(), c.getFaxNumber(), c.getAddress(), c.getCity(), c.getStateProvince(), c.getZipPostalCode(), c.getCountryRegion(), c.getWebPage(), c.getNotes()));
 		}	
 		return ret;
 	}
 	
+	public CustomersPageDTO findPage(Integer pageNum)
+	{
+		CustomersPageDTO ret;
+		
+		List<CustomersDTO> customersDTOList = new ArrayList<CustomersDTO>();	
+		CustomersDTO customersDTO;
+		for(Customers c : dao.findCustomersEntities(10, pageNum * 10))
+		{
+			customersDTO = new CustomersDTO(c.getId(), c.getCompany(), c.getLastName(), c.getFirstName(), c.getEmailAddress(), c.getJobTitle(), c.getBusinessPhone(), c.getHomePhone(), c.getMobilePhone(), c.getFaxNumber(), c.getAddress(), c.getCity(), c.getStateProvince(), c.getZipPostalCode(), c.getCountryRegion(), c.getWebPage(), c.getNotes());
+			customersDTOList.add(customersDTO);
+		}		
+		ret = new CustomersPageDTO(customersDTOList, dao.getCustomersCount());
+	
+		return ret;
+	}
 	
 
 }
