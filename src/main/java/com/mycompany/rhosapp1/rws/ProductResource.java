a8b2d13c7e81e8c74f09acc82be5880ca9b7ad2f
nleitefaria <nleitefaria@nleitefaria-HP-EliteBook>
2019-10-15 23:47:22 +0100

    clean

--- src/main/java/com/mycompany/rhosapp1/rws/ProductResource.java
+++ src/main/java/com/mycompany/rhosapp1/rws/ProductResource.java
@@ -11,17 +11,13 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.mycompany.rhosapp1.service.impl.ProductsServiceImpl;
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
 
 @Path("/product")
-@Api(value = "/product", description = "Products service")
 public class ProductResource {
 	
 	@GET
     @Path("/count")
     @Produces(MediaType.APPLICATION_JSON)
-    @ApiOperation(value = "count", notes = "Get products count")
     public Response count() {
 		ProductsServiceImpl service = new ProductsServiceImpl();
         JsonBuilderFactory factory = Json.createBuilderFactory(null);
@@ -32,7 +28,6 @@ public class ProductResource {
 	@GET
     @Path("/all")
     @Produces(MediaType.APPLICATION_JSON)
-    @ApiOperation(value = "all", notes = "Get all products")
     public Response findAll() {
 		ProductsServiceImpl service = new ProductsServiceImpl();
         return Response.status(200).entity(service.findAll()).build();
@@ -41,7 +36,6 @@ public class ProductResource {
 	@GET
 	@Path("/all/{pageNum}")
 	@Produces({MediaType.APPLICATION_JSON})
-	@ApiOperation(value = "all pagination", notes = "Get all products with pagination")
 	public Response findCustomersPage(@PathParam("pageNum") Integer pageNum)
 	{
 		ProductsServiceImpl service = new ProductsServiceImpl();
